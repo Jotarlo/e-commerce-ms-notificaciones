5cb4d6703ef8dcdac2ab32458b57d17f193967c6
Jeferson Arango L <jeferarlo2@gmail.com>
2021-10-04 18:00:08 -0500

    servidor de notificaciones en python

---
+++ environment.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 16:40:42 2021
+
+@author: Jota
+"""
+
+import os
+
+os.environ["SENDGRID_API_KEY"]="Acá va su API KEY de SENDGRID y no puede subirla a GITHUB"
+os.environ["SECURITY_HASH"]="ABC123"
+os.environ["email_from"]="jeferson.arango@ucaldas.edu.co"
