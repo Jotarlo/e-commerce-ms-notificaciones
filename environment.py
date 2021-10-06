bdfd49f3a2f5f12d4e2f8d6a93cfff4cfab31c79
Jeferson Arango L <jeferarlo2@gmail.com>
2021-10-06 18:11:23 -0500

    Funcionalidad de envío de sms y sus respectivas claves de entorno

--- environment.py
+++ environment.py
@@ -7,6 +7,8 @@ Created on Mon Oct  4 16:40:42 2021
 
 import os
 
-os.environ["SENDGRID_API_KEY"]="Acá va su API KEY de SENDGRID y no puede subirla a GITHUB"
+os.environ["SENDGRID_API_KEY"]="Acá debe ir el código de sendgrid"
 os.environ["SECURITY_HASH"]="ABC123"
 os.environ["email_from"]="jeferson.arango@ucaldas.edu.co"
+os.environ["TWILIO_ACCOUNT_SID"]="Acá debe ir el SID de twilio"
+os.environ["TWILIO_AUTH_TOKEN"]="Acá debe ir el Token de Twilio"
--- servidor.py
+++ servidor.py
@@ -10,6 +10,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from flask import request
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -33,6 +34,34 @@ def enviarCorreo():
             sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
             response = sg.send(message)
             print("enviado")
+            return "OK"
+        except Exception as e:
+            print(e.message)
+            return "KO"
+    else:
+        print("Hash error")
+        return "KO"
+    
+@app.route("/sms")
+def enviarSms():
+    destino = request.args.get("destino")
+    mensaje = request.args.get("mensaje")
+    hashString = request.args.get("hash")
+    if  hashString == os.environ.get("SECURITY_HASH"):
+        try:
+            account_sid = os.environ['TWILIO_ACCOUNT_SID']
+            auth_token = os.environ['TWILIO_AUTH_TOKEN']
+            client = Client(account_sid, auth_token)
+            
+            message = client.messages \
+                            .create(
+                                 body=mensaje,
+                                 from_="+13097044571",
+                                 to="+57"+destino
+                             )
+            
+            print(message.sid)
+            print("enviado sms")
             return "OK"
         except Exception as e:
             print(e.message)
